cc1c34b40674a6ac185c0a3bdef0917833111547
Chris Mountford <chris@think.net.au>
2015-01-15 11:24:37 +1100

    comments

--- ripradio.groovy
+++ ripradio.groovy
@@ -1,13 +1,10 @@
 #!/usr/bin/env groovy
 
+// groovy version of the same script just for lolz
 if (args.length < 1) {
-    println "usage: ripsoma.groovy <streamname> \n where <streamname> is dronezone, dubstep, groovesalad etc."
+    println "usage: ${System.properties['script.name']} <streamname> \n where <streamname> is dronezone, dubstep, groovesalad etc."
     System.exit(1)
 } else {
-    def dir = System.getenv().RIP_RADIO_HOME ?: "."
-    def p = "streamripper http://ice.somafm.com/${args[0]} -d $dir -u 'VLC/2.1.5 LibVLC/2.1.5' -q -r"
-    println p
-    
-    //def p = "streamripper http://ice.somafm.com/${args[0]} -d $dir -u 'VLC/2.1.5 LibVLC/2.1.5' -q -r".execute()
-    //p.consumeProcessOutput(System.out, System.err)
+    "streamripper http://ice.somafm.com/${args[0]} -d ${System.getenv().RIP_RADIO_HOME ?: "."} -u 'VLC/2.1.5 LibVLC/2.1.5' -q -r"
+        .execute().consumeProcessOutput(System.out, System.err)
 }
